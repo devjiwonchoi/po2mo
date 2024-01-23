8a408673361ce97188c69a795ca24ff90cdd46d5
Jiwon Choi <devjiwonchoi@gmail.com>
2024-01-23 20:10:28 +0900

    feat: initial attempt on cli (#42)

--- src/convert.ts
+++ src/convert.ts
@@ -1,6 +1,7 @@
-import { readFile, readdir, writeFile } from 'fs/promises'
-import { join } from 'path'
-import { mo, po } from 'gettext-parser'
+import { readFile, readdir, writeFile, stat } from 'fs/promises'
+import { join, resolve } from 'path'
+import { po, mo } from 'gettext-parser'
+import { logger } from './utils'
 
 type Po2MoConfig = {
   files: {
@@ -9,32 +10,21 @@ type Po2MoConfig = {
   }[]
 }
 
-async function getConfig(cwd: string) {
-  try {
-    const configFilePath = join(cwd, 'po2mo.json')
-    const config: Po2MoConfig = JSON.parse(
-      await readFile(configFilePath, 'utf-8')
-    )
-    return config
-  } catch (error) {
-    throw error
-  }
-}
-
-async function parsePoToMo(input: string, output: string) {
+async function convertPoToMo(input: string, output: string): Promise<void> {
   const poFile = await readFile(input, 'utf-8')
   const poData = po.parse(poFile)
   const moData = mo.compile(poData)
   await writeFile(output, moData)
 }
 
-async function getPoEntries(entry: string) {
+async function getPoEntries(entry: string, recursive: boolean) {
   const dirents = await readdir(entry, { withFileTypes: true })
   const poEntries: string[] = []
   dirents.forEach((dirent) => {
     const direntPath = join(entry, dirent.name)
     if (dirent.isDirectory()) {
-      getPoEntries(direntPath)
+      if (!recursive) return
+      getPoEntries(direntPath, recursive)
     }
     if (dirent.isFile() && dirent.name.endsWith('.po')) {
       poEntries.push(direntPath)
@@ -43,25 +33,71 @@ async function getPoEntries(entry: string) {
   return poEntries
 }
 
-export async function convert(cwd: string | null) {
-  if (!cwd) {
-    throw new Error('NOT_EXISTED')
-  }
-  const config = await getConfig(cwd)
+export async function po2mo({
+  config: configPath,
+  cwd: cwdParam,
+  input,
+  output,
+  recursive = false,
+}: {
+  config?: string
+  cwd?: string
+  input?: string | null
+  output?: string
+  recursive?: boolean
+}) {
+  const cwd = cwdParam ?? process.cwd()
 
-  const compileJobs = config.files.map(async ({ input, output }) => {
-    const isWildcardEntry = input.endsWith('/*')
-    if (isWildcardEntry) {
-      // /**/* or /*
-      const wildcardEntry = input.replace(/\/\*\*\/\*$|\/\*$/, '')
-      const poEntries = await getPoEntries(wildcardEntry)
-      return poEntries.map((poEntry) =>
-        parsePoToMo(poEntry, poEntry.replace('.po', '.mo'))
-      )
+  if (configPath) {
+    if (input || output || recursive) {
+      logger.warn('Cannot use --config with other options')
     }
 
-    return parsePoToMo(input, output)
-  })
+    configPath = configPath.endsWith('po2mo.json')
+      ? resolve(configPath)
+      : join(configPath, 'po2mo.json')
 
-  await Promise.all(compileJobs)
+    const config: Po2MoConfig = (await import(configPath)).default
+    const convertJobs: Promise<void>[] = config.files.map(({ input, output }) =>
+      convertPoToMo(join(cwd, input), join(cwd, output))
+    )
+
+    await Promise.all(convertJobs)
+    return
+  }
+
+  if (input) {
+    if (input.endsWith('.po')) {
+      if (output) {
+        if (output.endsWith('.mo')) {
+          await convertPoToMo(join(cwd, input), join(cwd, output))
+          return
+        }
+        if ((await stat(output)).isDirectory()) {
+          const filename = input.split('/').pop()?.replace('.po', '.mo')
+          await convertPoToMo(join(cwd, input), join(cwd, output, filename!))
+          return
+        }
+      }
+      await convertPoToMo(
+        join(cwd, input),
+        join(cwd, input.replace('.po', '.mo'))
+      )
+      return
+    }
+
+    const poEntries = await getPoEntries(join(cwd, input), recursive)
+    const convertJobs: Promise<void>[] = poEntries.map((poEntry) => {
+      if (output) {
+        if (output.endsWith('.mo')) {
+          throw new Error('Output path is not a directory')
+        }
+        return convertPoToMo(poEntry, join(cwd, output))
+      }
+      return convertPoToMo(poEntry, poEntry.replace('.po', '.mo'))
+    })
+
+    await Promise.all(convertJobs)
+    return
+  }
 }
