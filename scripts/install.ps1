203920e9e54472bde79d4700a0c8342eb96e5ff9
Jiwon Choi <devjiwonchoi@gmail.com>
2024-02-15 09:57:44 +0900

    feat: download via http, without Node.js (#79)
    * feat: add install.sh
    
    * chore: add pkg
    
    * chore: save ps1
    
    * feat: add scripts
    
    * refactor: comment out arch, add version
    
    * refactor: set path env
    
    * refactor: set po2mo bin for windows

---
+++ scripts/install.ps1
@@ -0,0 +1,125 @@
+#!/usr/bin/env pwsh
+
+# Stop executing script on any error
+$ErrorActionPreference = 'Stop'
+# Do not show download progress
+$ProgressPreference = 'SilentlyContinue'
+
+# Taken from https://stackoverflow.com/a/34559554/6537420
+function New-TemporaryDirectory {
+  $parent = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+$platform = $null
+# $architecture = $null
+$po2moName = $null
+
+# PowerShell versions before 6.* were only for Windows OS
+if ($PSVersionTable.PSVersion.Major -eq 5) {
+  $platform = 'win'
+}
+
+if ($PSVersionTable.PSVersion.Major -ge 6) {
+  if ($PSVersionTable.Platform -eq 'Unix') {
+    if ($PSVersionTable.OS -like 'Darwin*') {
+      $platform = 'macos'
+    }
+
+    if ($PSVersionTable.OS -like 'Linux*') {
+      $platform = 'linux'
+    }
+
+    # PowerShell does not seem to have normal cmdlets for retrieving system information, so we use UNAME(1) for this.
+    # $arch = uname -m
+    # switch -Wildcard ($arch) {
+    #   'x86_64' { $architecture = 'x64'; Break }
+    #   'amd64' { $architecture = 'x64'; Break }
+    #   'armv*' { $architecture = 'arm'; Break }
+    #   'arm64' { $architecture = 'arm64'; Break }
+    #   'aarch64' { $architecture = 'arm64'; Break }
+    # }
+
+    # 'uname -m' in some cases mis-reports 32-bit OS as 64-bit, so double check
+    # if ([System.Environment]::Is64BitOperatingSystem -eq $false) {
+    #   if ($architecture -eq 'x64') {
+    #     $architecture = 'i686'
+    #   }
+
+    #   if ($architecture -eq 'arm64') {
+    #     $architecture = 'arm'
+    #   }
+    # }
+
+    $po2moName = "po2mo"
+  }
+
+  if ($PSVersionTable.Platform -eq 'Win32NT') {
+    $platform = 'win'
+  }
+}
+
+if ($platform -eq 'win') {
+  # if ([System.Environment]::Is64BitOperatingSystem -eq $true) {
+  #   $architecture = 'x64'
+  # }
+
+  # if ([System.Environment]::Is64BitOperatingSystem -eq $false) {
+  #   $architecture = 'i686'
+  # }
+
+  $po2moName = "po2mo.exe"
+}
+
+if ($null -eq $platform) {
+  Write-Error "Platform could not be determined! Only Windows, Linux and MacOS are supported."
+}
+
+# switch ($architecture) {
+#   'x64' { ; Break }
+#   'arm64' { ; Break }
+#   Default {
+#     Write-Error "Sorry! po2mo currently only provides pre-built binaries for x86_64/arm64 architectures."
+#   }
+# }
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$versionJson = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/devjiwonchoi/po2mo/main/package.json"
+$version = $versionJson.version
+
+$preferredVersion = $null
+
+if ($null -ne $env:po2mo_VERSION -and $env:po2mo_VERSION -ne "") {
+  $preferredVersion = $env:po2mo_VERSION
+}
+
+if ($null -ne $preferredVersion) {
+  $version = $preferredVersion
+}
+
+Write-Host "==> " -NoNewline -ForegroundColor Blue
+Write-Host "Downloading po2mo binaries v$version" -ForegroundColor White
+
+$po2moHome = $HOME + "/AppData/Local/.po2mo"
+mkdir $po2moHome -ea 0
+$po2moFile = (Join-Path $po2moHome $po2moName)
+$archiveUrl="https://github.com/devjiwonchoi/po2mo/releases/download/v$version/po2mo-$platform"
+if ($platform -eq 'win') {
+  $archiveUrl="$archiveUrl.exe"
+}
+
+Invoke-WebRequest $archiveUrl -OutFile $po2moFile -UseBasicParsing
+
+if ($platform -ne 'win') {
+  chmod +x $po2moFile
+}
+
+if ($platform -eq 'win') {
+  [Environment]::SetEnvironmentVariable('PATH', $env:PATH, [EnvironmentVariableTarget]::User)
+}
+
+Start-Process -FilePath $po2moFile -ArgumentList "-h" -NoNewWindow -Wait -ErrorAction Continue
+
+Write-Host "Thank you for downloading po2mo!" -ForegroundColor Blue
---
+++ scripts/install.sh
@@ -0,0 +1,120 @@
+#!/bin/sh
+
+# From https://github.com/Homebrew/install/blob/master/install.sh
+abort() {
+  printf "%s\n" "$@"
+  exit 1
+}
+
+# string formatters
+if [ -t 1 ]; then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+tty_mkbold() { tty_escape "1;$1"; }
+tty_blue="$(tty_mkbold 34)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
+ohai() {
+  printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$1"
+}
+
+# End from https://github.com/Homebrew/install/blob/master/install.sh
+
+download() {
+  if command -v curl >/dev/null 2>&1; then
+    curl -fsSL "$1"
+  else
+    wget -qO- "$1"
+  fi
+}
+
+is_glibc_compatible() {
+  getconf GNU_LIBC_VERSION >/dev/null 2>&1 || ldd --version >/dev/null 2>&1 || return 1
+}
+
+detect_platform() {
+  local platform
+  platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
+  case "${platform}" in
+  linux)
+    if is_glibc_compatible; then
+      platform="linux"
+    else
+      platform="linuxstatic"
+    fi
+    ;;
+  darwin) platform="macos" ;;
+  windows) platform="win" ;;
+  esac
+
+  printf '%s' "${platform}"
+}
+
+detect_arch() {
+  local arch
+  arch="$(uname -m | tr '[:upper:]' '[:lower:]')"
+
+  case "${arch}" in
+  x86_64 | amd64) arch="x64" ;;
+  armv*) arch="arm" ;;
+  arm64 | aarch64) arch="arm64" ;;
+  esac
+
+  # `uname -m` in some cases mis-reports 32-bit OS as 64-bit, so double check
+  if [ "${arch}" = "x64" ] && [ "$(getconf LONG_BIT)" -eq 32 ]; then
+    arch=i686
+  elif [ "${arch}" = "arm64" ] && [ "$(getconf LONG_BIT)" -eq 32 ]; then
+    arch=arm
+  fi
+
+  case "$arch" in
+  x64*) ;;
+  arm64*) ;;
+  *) return 1 ;;
+  esac
+  printf '%s' "${arch}"
+}
+
+download_and_install() {
+  local platform arch version_json version archive_url po2mo_home
+  platform="$(detect_platform)"
+  # arch="$(detect_arch)" || abort "Sorry! po2mo currently only provides pre-built binaries for x86_64/arm64 architectures."
+  if [ -z "${PO2MO_VERSION}" ]; then
+    version_json="$(download "https://raw.githubusercontent.com/devjiwonchoi/po2mo/main/package.json")" || abort "Download Error!"
+    version="$(printf '%s' "${version_json}" | tr '{' '\n' | awk -F '"' '/version/ { print $4 }')"
+  else
+    version="${PO2MO_VERSION}"
+  fi
+
+  archive_url="https://github.com/devjiwonchoi/po2mo/releases/download/v${version}/po2mo-${platform}"
+  if [ "${platform}" = "win" ]; then
+    archive_url="${archive_url}.exe"
+  fi
+
+  # install to PO2MO_HOME, defaulting to ~/.po2mo
+  po2mo_home="$HOME/.po2mo"
+  mkdir -p "$po2mo_home" || abort "Mkdir Error!"
+  trap 'rm -rf "$po2mo_home"' EXIT INT TERM HUP
+
+  ohai "Downloading po2mo binaries v${version}"
+  # download the binary to the specified directory
+  download "$archive_url" >"$po2mo_home/po2mo" || return 1
+  chmod +x "$po2mo_home/po2mo"
+  SHELL="$SHELL" "$po2mo_home/po2mo" -h || return 1
+
+  printf "${tty_blue}To run po2mo on the current terminal, enter: ${tty_reset}"
+
+  if [ "${platform}" = "macos" ]; then
+    echo "export PATH=\"\$PATH:$po2mo_home\"" >>~/.zshrc
+    printf "${tty_bold}source ~/.zshrc${tty_reset}\n"
+  elif [ "${platform}" = "linux" ]; then
+    echo "export PATH=\"\$PATH:$po2mo_home\"" >>~/.bashrc
+    printf "${tty_bold}source ~/.bashrc${tty_reset}\n"
+  fi
+}
+
+download_and_install || abort "Install Error!"
