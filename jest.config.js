ea8bf1f76c836435f0c324217c5ebdf2d1a7fd62
devjiwonchoi <devjiwonchoi@gmail.com>
2024-04-10 05:38:45 +0900

    v2.0.4

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
-  },
-  testEnvironment: 'node',
-  testTimeout: 10000,
-  verbose: true,
-}
